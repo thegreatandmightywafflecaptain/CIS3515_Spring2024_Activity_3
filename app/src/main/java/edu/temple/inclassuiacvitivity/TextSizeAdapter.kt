1eb982c76ec1fedb30b95a8cd4909667b4a43a46
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2024-02-08 12:12:35 -0500

    Made it so that each number in the drop down menu is the font size of the number it's representing. Before anything is selected, the number displayed has a fontsize of 48 by default.

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,39 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
-
+class TextSizeAdapter(_context: Context, _numberList : Array<Int>) : BaseAdapter() {
+    private val context = _context
+    private val numberList = _numberList
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return numberList.size
+
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return numberList[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = getItem(position).toString()
+        textView.textSize = 48f
+        textView.setPadding(5, 10, 0, 10)
+        return textView
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        return getView(position, convertView, parent).apply{
+            (this as TextView).textSize = getItem(position).toString().toFloat()
+        }
     }
 
 }
